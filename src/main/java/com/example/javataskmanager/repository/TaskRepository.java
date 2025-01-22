ad91b7bf343522fcbae827a1c9454cd6ae0fff99
Ken Bjelosevic <kbjelose1992@gmail.com>
2025-01-22 10:03:11 -0700

    Forgot to add Repository comments.

--- src/main/java/com/example/javataskmanager/repository/TaskRepository.java
+++ src/main/java/com/example/javataskmanager/repository/TaskRepository.java
@@ -3,5 +3,14 @@ package com.example.javataskmanager.repository;
 import com.example.javataskmanager.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//Data Access Layer
+//Extending JPARepository enables built-in methods.
+//save(Task entity)
+//findById(Long id)
+//findAll()
+//deleteById(Long id)
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    //Stays empty because Spring Data JPA unless I need to add custom query methods.
+    //JPA is Java Persistence API.
 }
